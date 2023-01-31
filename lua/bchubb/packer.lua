ce521b836245a75f9ec1e5da1726ba3190633e4e
Billy Chubb <billy@bchubb.co.uk>
2023-01-31 09:08:29 +0000

    added trouble kind of

--- lua/bchubb/packer.lua
+++ lua/bchubb/packer.lua
@@ -34,6 +34,17 @@ return require('packer').startup(function(use)
     -- Snippets
     {'L3MON4D3/LuaSnip'},             -- Required
     {'rafamadriz/friendly-snippets'}, -- Optional
-  }
+}
+}
+use {
+    "folke/trouble.nvim",
+    requires = "nvim-tree/nvim-web-devicons",
+    config = function()
+        require("trouble").setup {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        }
+    end
 }
 end)
