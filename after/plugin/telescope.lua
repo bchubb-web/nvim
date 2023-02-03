5704c31f076a94b8d691bcc33943df62cacd386a
Billy Chubb <billy@bchubb.co.uk>
2023-02-03 21:50:32 +0000

    commit?

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,3 +1,5 @@
 local builtin = require('telescope.builtin')
+builtin.find_files{ on_complete = {function () vim.cmd("stopinsert") end}}
+builtin.git_files{ on_complete = {function () vim.cmd("stopinsert") end}}
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
