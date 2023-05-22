9eb3b26d2c0bc8987fd80e5e53d9d4a251432aca
bchubb-web <billy@bchubb.co.uk>
2023-05-22 19:39:44 +0100

    harpoon stuff

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,16 +1,10 @@
 require("harpoon").setup()
-
-local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
-local tmux = require("harpoon.tmux")
+local mark = require("harpoon.mark")
 
 vim.keymap.set("n", "mf", mark.add_file)
-vim.keymap.set("n", "hp", ui.toggle_quick_menu)
-
-vim.keymap.set("n", "∆", function() ui.nav_file(1) end)
-vim.keymap.set("n", "˚", function() ui.nav_file(2) end)
-vim.keymap.set("n", "¬", function() ui.nav_file(3) end)
-vim.keymap.set("n", "…", function() ui.nav_file(4) end)
+vim.keymap.set("n", "H", ui.toggle_quick_menu)
+vim.keymap.set("n", "fj", ui.toggle_quick_menu)
 
 vim.keymap.set("n", "<", function() ui.nav_prev() end)
 vim.keymap.set("n", ">", function() ui.nav_next() end)
