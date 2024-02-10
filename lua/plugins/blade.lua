f55e2ec15afc2071d8c92a0ccd0f8eb3b5446986
bchubb-web <billy@bchubb.co.uk>
2024-02-10 02:30:54 +0000

    [amend] got blade highlighting working

---
+++ lua/plugins/blade.lua
@@ -0,0 +1,10 @@
+return {
+    {
+        'EmranMR/tree-sitter-blade',
+        lazy=false,
+    },
+    {
+        'jwalton512/vim-blade',
+        lazy=false,
+    }
+}
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,17 +1,25 @@
 return {
-    {
-        'nvim-treesitter/nvim-treesitter',
-        lazy=false,
-        build = ':TSUpdate',
-        config = function () 
-            local configs = require("nvim-treesitter.configs")
+    'nvim-treesitter/nvim-treesitter',
+    lazy=false,
+    build = ':TSUpdate',
+    config = function () 
 
-            configs.setup({
-                ensure_installed = { "c", "cpp", "lua", "vim", "vimdoc", "query", "javascript", "html", "php", "blade", "vim-blade" },
-                sync_install = false,
-                highlight = { enable = true },
-                indent = { enable = true },  
-            })
-        end
-    }
+        require("nvim-treesitter.configs").setup({
+            ensure_installed = { "c", "cpp", "lua", "vim", "vimdoc", "query", "javascript", "html", "php", "blade" },
+            sync_install = false,
+            highlight = { enable = true },
+            indent = { enable = true },  
+        })
+
+        local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+
+        parser_config.blade = {
+            install_info = {
+                url = "https://github.com/EmranMR/tree-sitter-blade",
+                files = { "src/parser.c" },
+                branch = "main",
+            },
+            filetype = "blade",
+        }
+    end
 }
